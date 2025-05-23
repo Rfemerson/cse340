14adf880b6cc8c6be9090dc0f99b4c1bfbae9dc0
Emerson Ferreira Rodrigues <104386868+Rfemerson@users.noreply.github.com>
2025-05-23 18:20:23 -0300

    Some fix

--- utilities/index.js
+++ utilities/index.js
@@ -21,8 +21,6 @@ Util.getNav = async function (req, res, next) {
   return list
 }
 
-module.exports = Util
-
 /* **************************************
 * Build the classification view HTML
 * ************************************ */
@@ -34,7 +32,7 @@ Util.buildClassificationGrid = async function(data){
         grid += '<li>'
         grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
         + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-        + 'details"><img src="' + vehicle.inv_thumbnail 
+        + 'details"><img src="/' + vehicle.inv_thumbnail 
         +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
         +' on CSE Motors" /></a>'
         grid += '<div class="namePrice">'
@@ -55,3 +53,12 @@ Util.buildClassificationGrid = async function(data){
     }
     return grid
   }
+
+/* ****************************************
+* Middleware For Handling Errors
+* Wrap other function in this for 
+* General Error Handling
+**************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
+module.exports = Util
